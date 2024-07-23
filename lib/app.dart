30887514e291f8b6f4988075bd49f4d172a3f5a2
Fexxix <fexxix@gmail.com>
2024-07-23 15:49:01 +0500

    chore: setup utils, theme and packages

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:shoppe/utils/theme/theme.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      themeMode: ThemeMode.system,
+      home: const Placeholder(),
+    );
   }
 }
---
+++ lib/utils/constants/enums.dart
@@ -0,0 +1,13 @@
+enum OrderStatus { processing, shipped, delivered }
+
+enum PaymentMethods {
+  paypal,
+  googlePay,
+  applePay,
+  visa,
+  masterCard,
+  creditCard,
+  paystack,
+  razorPay,
+  paytm
+}
